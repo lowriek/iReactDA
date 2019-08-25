49eb729ba22765ccd10d2992d1e5dff9df4818b1
Kate Lowrie <lowriek@bc.edu>
2019-08-25 11:39:21 -0400

    added sql for database initialization, starting on php

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,8 +2,9 @@ use ireact;
 DROP TABLE IF EXISTS composition;
 CREATE TABLE composition (
   compositionID int(11) NOT NULL auto_increment,
-  name varchar(100) NOT NULL,
-  category varchar (100) NOT NULL,
+  composertname varchar(100) NOT NULL,
+  compositionname varchar(100) NOT NULL,
+  collectioneabled boolean,
   PRIMARY KEY  (`compositionID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -16,7 +17,7 @@ CREATE TABLE reactiondata (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO composition VALUES
-(1, "Yu-Hui Chang", "Ode to Kate"
+(1, "Yu-Hui Chang", "Ode to Kate", FALSE
 );
 
 INSERT INTO reactiondata VALUES
